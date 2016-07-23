7080ed809f2562915d41c56de70f4829254cc9ce
Erwinovitch <lotr_erwin@live.nl>
2016-07-23 15:37:32 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,13 +5,19 @@ set -e
 cd /tmp
 git clone git://github.com/Erwinovitch/msfonts.git
 
+echo""
 echo "copying fonts to /usr/share/fonts/TTF/..."
+echo ""
 mkdir -p /usr/share/fonts/TTF/
-cp /tmp/msfonts/fonts/* /usr/share/fonts/TTF/ -rf
+cp /tmp/msfonts/fonts/* /usr/share/fonts/TTF/ -rfv
 
+echo ""
 echo "updating all caches..."
-fc-cache -vf
+echo ""
+fc-cache -fv
 mkfontscale
 mkfontdir
 
+echo ""
 echo "done."
+echo ""
